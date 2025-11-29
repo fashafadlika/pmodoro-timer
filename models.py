85803fa3b8908cddfb08327cb606d160afe5b4d1
znva <fashafadlika@gmail.com>
2025-11-29 22:02:41 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
+from typing import Optional
 from pydantic import BaseModel
 
 class PomodoroSettings(BaseModel):
     mode: str
-    duration: int
+    duration: Optional[int] = None  # Bisa None untuk resume
     rounds: int
