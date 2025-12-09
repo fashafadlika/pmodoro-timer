62c648b0ac8bed111a56a97295d9eb448df74464
znva <fashafadlika@gmail.com>
2025-12-09 15:07:38 +0700

    Delete models.py

--- models.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-class PomodoroSettings(BaseModel):
-    mode: str
-    duration: Optional[int] = None  # Bisa None untuk resume
-    rounds: int
